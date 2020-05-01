36c32ecf3570b982f59a88d4ee9f91a9ba48759e
litetex <litetex.dev@gmail.com>
2020-05-01 20:27:39 +0200

    Init

---
+++ PullDetachedRemote/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PullDetachedRemote
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
